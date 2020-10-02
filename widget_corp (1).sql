f13289b21646a04881f90d25459431ae2dd9014b
anmolchandelCO180309 <42858993+anmolchandelCO180309@users.noreply.github.com>
2020-10-03 01:34:43 +0530

    Add files via upload

---
+++ widget_corp (1).sql
@@ -0,0 +1,143 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 19, 2020 at 07:23 PM
+-- Server version: 10.1.33-MariaDB
+-- PHP Version: 7.2.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `widget_corp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `hashed_password` varchar(60) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `username`, `hashed_password`) VALUES
+(1236, 'web', '2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pages`
+--
+
+CREATE TABLE `pages` (
+  `id` int(11) NOT NULL,
+  `subject_id` int(11) NOT NULL,
+  `menu_name` varchar(30) NOT NULL,
+  `position` int(3) NOT NULL,
+  `visible` tinyint(1) NOT NULL,
+  `content` text
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pages`
+--
+
+INSERT INTO `pages` (`id`, `subject_id`, `menu_name`, `position`, `visible`, `content`) VALUES
+(9, 26, 'Our history', 2, 1, 'Founded in 1988 by two enterprising engineers....'),
+(8, 26, 'Our mission ', 1, 1, 'Our mission has always been...'),
+(3, 2, 'Large Widgets', 1, 1, 'Our large widgets have to be seen to be believed...'),
+(4, 2, 'Small Widgets', 2, 1, 'They say big things come in small packages...'),
+(5, 3, 'Retrofitting', 1, 1, 'We love to replace widgets...'),
+(6, 3, 'Certification', 2, 1, 'We can certify any widget, not just our own...'),
+(11, 29, 'Project', 1, 0, 'This Content Management system is created with PHP and MySQL databases also with the help of languages like HTML CSS and javascript. \r\nThe system is consisting of 2 areas one is\r\n Public (the general area where users can view the items) and the Administrator are (the administrative area where \r\nonly authorized users can access and maintain the information on subjects and pages which are stored in the backend MySQL database).');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subjects`
+--
+
+CREATE TABLE `subjects` (
+  `id` int(11) NOT NULL,
+  `menu_name` varchar(30) NOT NULL,
+  `position` int(3) NOT NULL,
+  `visible` tinyint(1) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `subjects`
+--
+
+INSERT INTO `subjects` (`id`, `menu_name`, `position`, `visible`) VALUES
+(26, 'About Widget Corp', 1, 1),
+(2, 'Products', 2, 1),
+(3, 'Services', 3, 1),
+(29, 'web assignment', 4, 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pages`
+--
+ALTER TABLE `pages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `subject_id` (`subject_id`);
+
+--
+-- Indexes for table `subjects`
+--
+ALTER TABLE `subjects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1237;
+
+--
+-- AUTO_INCREMENT for table `pages`
+--
+ALTER TABLE `pages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `subjects`
+--
+ALTER TABLE `subjects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
